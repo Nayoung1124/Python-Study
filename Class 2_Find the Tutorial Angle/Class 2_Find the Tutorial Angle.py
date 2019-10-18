05a438846d29445738849a548ab605d4ac0ad978
Nayoung1124 <na_young_1124@naver.com>
2019-10-18 13:32:31 +0900

    first commit

---
+++ Class 2_Find the Tutorial Angle/Class 2_Find the Tutorial Angle.py
@@ -0,0 +1,27 @@
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __sub__(self, no):
+        x = self.x - no.x
+        y = self.y - no.y
+        z = self.z - no.z
+        return Points(x, y, z)
+
+    def dot(self, no):
+        x = self.x * no.x
+        y = self.y * no.y
+        z = self.z * no.z
+        return x + y + z
+
+    def cross(self, no):
+        x = self.y * no.z - self.z * no.y
+        y = self.z * no.x - self.x * no.z
+        z = self.x * no.y - self.y * no.x
+        return Points(x, y, z)
+        
+    def absolute(self):
+        return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
+
