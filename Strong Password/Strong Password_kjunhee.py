d210824d33392c98dafdf4f84d26c38dab915389
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2019-10-25 15:59:39 +0900

    Create Strong Password_kjunhee.py

---
+++ Strong Password/Strong Password_kjunhee.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def minimumNumber(n, password):
+    
+    numbers = "0123456789"
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special_characters = "!@#$%^&*()-+"
+    
+    len_password=len(password)
+    list_password=list(password)
+    score=0
+
+    if len_password < 6:
+        a = 6-len_password
+        return a
+    elif numbers not in list_password:
+        score=1
+        return score
+    elif lower_case not in list_password:
+        score=1
+        return score 
+    elif upper_case not in list_password:
+        score=1
+        return score
+    elif special_characters not in list_password:
+        score=1
+        return score
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
