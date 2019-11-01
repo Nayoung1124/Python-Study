c1d46b4a628af5e022a381333bcd685c38dbeebd
gek7537 <56713634+gek7537@users.noreply.github.com>
2019-11-01 17:13:19 +0900

    Create Electronics Shop_junga.py

---
+++ Electronics Shop/Electronics Shop_junga.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b, n ,m):
+
+    sum_=[-1]
+    for i in range(n):
+        for j in range(m):
+            if keyboards[i]+drives[j] <= b:
+                sum_.append(keyboards[i]+drives[j])    
+    return(max(sum_))
+
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b, n ,m)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
