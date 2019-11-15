b34fedc0e802b40134fbc34ea573e3f93492dc9d
gek7537 <56713634+gek7537@users.noreply.github.com>
2019-11-15 14:24:47 +0900

    Create Picking Numbers_JA.py

---
+++ [6] Picking Numbers/Picking Numbers_JA.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    A = 0
+    B = list(set(a))
+
+    for i in B:
+        n = a.count(i) + a.count(i+1)
+        if n > A:
+            A = n
+    
+    return A
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
