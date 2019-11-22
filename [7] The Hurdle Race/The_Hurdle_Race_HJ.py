8c97038617c7bbff9ed81d112eb986396a0a6a99
lxzun <41243762+lxzun@users.noreply.github.com>
2019-11-22 15:59:57 +0900

    Create The_Hurdle_Race_HJ.py

---
+++ [7] The Hurdle Race/The_Hurdle_Race_HJ.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, height):
+    m = max(height)
+    return m-k if m>k else 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
