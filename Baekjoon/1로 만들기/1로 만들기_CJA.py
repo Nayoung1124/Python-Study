7f54c5397f27074f555c39d1b67f8e186d04f2a1
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-02-26 14:40:39 +0900

    Create 1로 만들기_CJA.py

---
+++ Baekjoon/1로 만들기/1로 만들기_CJA.py
@@ -0,0 +1,13 @@
+N = int(input())
+dp = [0]*(N+1)
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    if i%2 == 0 :
+        if dp[i] > dp[i//2] + 1:
+            dp[i] = dp[i//2] + 1
+    if i%3 == 0: 
+        if dp[i] > dp[i//3] + 1:
+            dp[i] = dp[i//3] + 1
+
+print(dp[N])
