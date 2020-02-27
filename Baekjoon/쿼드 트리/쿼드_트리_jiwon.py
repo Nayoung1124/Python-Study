a6c71cf0cdb71f4382691ebf9d8b2d7518c23da3
treejw <42428487+treejw@users.noreply.github.com>
2020-02-27 15:42:38 +0900

    jiwon update

--- Baekjoon/쿼드 트리/쿼드_트리_jiwon.py
+++ Baekjoon/쿼드 트리/쿼드_트리_jiwon.py
@@ -5,19 +5,19 @@ input_func = sys.stdin.readline
 # 재귀함수
 def Quad_Tree(N, Map):
     
-    # 재귀함수 탈출조건: 현재 입력으로 들어온 Map의 길이가 0인 경우
+    # 재귀함수 탈출조건: 현재 입력으로 들어온 Map의 길이가 0인 경우.
     if len(Map) == 0:
         return
     
-    # 현재 입력으로 들어온 Map의 요소들이 모두 1인경우 '1' 반환
+    # 현재 입력으로 들어온 Map의 요소들이 모두 1인경우 '1' 반환.
     if '0' not in sum(Map, []):
         return '1'
     
-    # 현재 입력으로 들어온 Map의 요소들이 모두 0인경우 '0' 반환
+    # 현재 입력으로 들어온 Map의 요소들이 모두 0인경우 '0' 반환.
     elif '1' not in sum(Map, []):
         return '0'
     
-    # 그 외의 경우 Map을 4분할하여 Quad_Tree함수 다시 호출
+    # 그 외의 경우 Map을 4분할하여 Quad_Tree함수 다시 호출.
     string = '({}{}{}{})'.format(
         Quad_Tree(N//2, [x[:N//2] for x in Map[:N//2]]),
         Quad_Tree(N//2, [x[N//2:] for x in Map[:N//2]]),
